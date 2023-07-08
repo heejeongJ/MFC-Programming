35024128c917752ea5399e83f278348233b02b82
heejeongJ <heeej920@naver.com>
2023-07-08 22:39:00 +0900

    Add +3
    Add +3

---
+++ 2.1 마우스 키보드 이벤트 활용 스톱워치/StopWatch/2021111408.h
@@ -0,0 +1,32 @@
+﻿
+// 2021111408.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CMy2021111408App:
+// 이 클래스의 구현에 대해서는 2021111408.cpp을(를) 참조하세요.
+//
+
+class CMy2021111408App : public CWinApp
+{
+public:
+	CMy2021111408App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy2021111408App theApp;
